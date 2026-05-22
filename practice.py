c5e41a161c48d144f7eb135b4bb1aaa26284e4c2
Umarastrophysics <umarlaughingextermin@gmail.com>
2026-05-22 22:17:39 +0500

    Update practice.py
    A Python script that classifies cosmic bodies (planets, stars, neutron stars, and black holes) based on their mass in solar masses

--- practice.py
+++ practice.py
@@ -1,1 +1,13 @@
-print("hello")
+mass = float(input("put mass in (solar masses) find type of cosmic body"))
+if mass < 0.013:
+    print("it is a rocky planet or a gas giant")
+elif mass < 0.08:
+    print("it is a brown drarf")
+elif mass < 8:
+    print("it is a small or medium star")
+elif mass < 18:
+    print("it will collapse into a neutron star")
+elif mass >= 18:
+    print("it will collapse into a black hole")
+else:
+    print("uknown cosmic object")
