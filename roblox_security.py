bb8d36712489208b6738d265a0b5a23523b46aa5
Umarastrophysics <umarlaughingextermin@gmail.com>
2026-05-24 23:28:54 +0500

    Update roblox_security.py
    a basic login system

--- roblox_security.py
+++ roblox_security.py
@@ -1,9 +1,10 @@
 """
     basic
-         roblox secourity system
-                                login system
-                                            via pasword
-                                                 """
+           roblox secourity system
+                                   login system
+                                                 via pasword
+                                                             and
+                                                                 f-strings as well"""
 players = ["jimmy","timmy","mummy","bummy","billy","silly"]
 players.extend (["karl","nolan"])
 def secourity_alarm():
@@ -12,10 +13,10 @@ def secourity_alarm():
 id = input("what is your roblox id: ")
 password = (input("what is your password"))
 if id == "roblox" and password == "gemini":
-    print("you have owner panel and ability to ban players")
+    print(f"you have owner panel and ability to ban players id: {id}")
 elif id == "admin1234" and password == "gpt":
-    print ("you are an admin have abilty to ban players admin panel")
-elif id == "players" and password == "1234":
-    print("welcome players")
+    print (f"you are an admin have abilty to ban players admin panel id: {id}")
+elif id in players and password == "1234":
+    print(f"welcome id: {id}")
 else:
     secourity_alarm()
